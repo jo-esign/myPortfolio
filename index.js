46cddf2fdf6520c8fd7cae71b6391a9b8799d67e
Eghogho649 <joymatthew100@gmail.com>
2022-06-30 13:12:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+document.querySelector('.second-button').addEventListener('click', function () {
+
+    document.querySelector('.animated-icon2').classList.toggle('open');
+    });
+
+    
