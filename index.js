c25d3124c33dbde5fa2a8b8c8a045d6d56058e47
Eghogho649 <joymatthew100@gmail.com>
2022-07-06 15:27:51 +0100

    edited

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ function myFunction() {
    element.classList.toggle("dark-mode");
 }
 
-document.querySelector('.second-button').addEventListener('click', function () {
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-    document.querySelector('.animated-icon2').classList.toggle('open');
-    });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
 
-    
+function scrollFunction() {
+if (
+document.body.scrollTop > 50 ||
+document.documentElement.scrollTop > 50
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+   
