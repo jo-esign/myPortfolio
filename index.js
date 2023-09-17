f536964c32efa4d737d0f036feeff2f6b211f2f3
Joy Matthew <joy.matthew09@icloud.com>
2023-09-17 06:39:03 +0100

    index

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ document.querySelector('.second-button').addEventListener('click', function () {
     document.querySelector('.animated-icon2').classList.toggle('open');
     });
 
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
 
 
- 
+    let testButton = document.getElementById("btn");
+testButton.addEventListener("click", () => {
+  testButton.classList.toggle('clicked');
+})
